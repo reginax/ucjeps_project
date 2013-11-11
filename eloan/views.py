66e3b5c9abf23814d49ae687acabda708c1c7982
Rick Jaffe <rjaffe@berkeley.edu>
2013-11-10 20:18:05 -0800

    NOJIRA (djangoEloan): Fixed Title and Timestamp on various result pages

--- eloan/views.py
+++ eloan/views.py
@@ -40,7 +40,7 @@ config = cspace_django_site.getConfig()
 
 # Static string parts for publicsearch (Solr) query
 SOLRSERVER = 'http://localhost:8983/solr'
-SOLRQUERYPARAMCSID = 'csid'
+# SOLRQUERYPARAMCSID = 'csid'
 SOLRQUERYPARAMACCESSION = 'accession'
 SOLRCORE = 'ucjeps-metadata'
 
@@ -51,19 +51,23 @@ SEARCHRESULTS = {}
 
 TITLE = 'eLoan'
 
-#@login_required()
+
 def eloan(request):
+    """
+
+    :param request:
+    :return:
+    """
     if 'kw' in request.GET and request.GET['kw']:
         eloanNum = urllib.quote_plus(request.GET['kw'])
         eloanNum = str(eloanNum)
 
         if 'recType' in request.GET and request.GET['recType']:
-           recType = urllib.quote_plus(request.GET['recType'])
-           recType = str(recType)
-        #TODO Make this else clause error message work so value can be entered by user and form can be generalizable.
-        #else send error message: "Please enter the type of record in which the keyword can be found"
-        # Do this as Javascript validation?
-        # TEMP FIX
+            recType = urllib.quote_plus(request.GET['recType'])
+            recType = str(recType)
+
+        # Record type hard-coded for now in eloan.html. Generalize?
+        # This else clause in not really needed.
         else:
             recType = 'loansout'
 
@@ -72,7 +76,7 @@ def eloan(request):
         #################################################################
 
         #  get eloan list-item
-        asquery = '%s?as=%s_common%%3AloanOutNumber%%3D%%27%s%%27&wf_deleted=false'  % (recType, recType, eloanNum,)
+        asquery = '%s?as=%s_common%%3AloanOutNumber%%3D%%27%s%%27&wf_deleted=false' % (recType, recType, eloanNum,)
 
         expectedmimetype = 'application/xml'
 
@@ -82,15 +86,14 @@ def eloan(request):
 
         # To grab everything: loinfo = loanoutlistXML.find('.//list-item')
         # To grab just the Loanout CSID:
-        try:
-            locsid = loanoutlistXML.find('.//list-item/csid')
-            locsid = locsid.text
-        except:
-            errMsg = "Could not find the loan %s. Please try another." % eloanNum
-            print(errMsg)
-            # TODO: FIX THIS - Should write simple message on screen: "Could not find that loan. Please try another" and ...
-            # TODO (cont.) return browser to http://localhost:8000/public/eloan/ or include a link back.
-            raise Exception(errMsg)
+
+        locsid = loanoutlistXML.find('.//list-item/csid')
+        if locsid is None:
+            errMsg = 'Error: We could not find the loan \'%s.\' Please try another.' % eloanNum
+            return render(request, 'eloan.html',
+                          {'results': errMsg, 'displayType': 'error'}
+            )
+        locsid = locsid.text
 
         # get eloan record
         loquery = '%s/%s' % (recType, locsid)
@@ -103,18 +106,18 @@ def eloan(request):
         loaninfo = []
         loaninfo.append(eloanNum)
 
-        borcontact = getfromXML(loanoutXML,'.//borrowersContact')
+        borcontact = getfromXML(loanoutXML, './/borrowersContact')
         loaninfo.append(borcontact)
 
-        lodate = getfromXML(loanoutXML,'.//loanOutDate')
+        lodate = getfromXML(loanoutXML, './/loanOutDate')
         loaninfo.append(lodate)
 
         # Get related collection objects; some pertinent info can be gleaned from list results
         # Record type hard-coded for now. Generalize this?
-        roquery = '%s?sbj=%s&objType%%3D%%27%s' %  ('relations', locsid, 'collectionobject')
+        roquery = '%s?sbj=%s&objType%%3D%%27%s' % ('relations', locsid, 'collectionobject')
 
         # Make authenticated connection to ucjeps.cspace...
-        rolistdata = get_entity(request, roquery,  expectedmimetype).content
+        rolistdata = get_entity(request, roquery, expectedmimetype).content
         relatedObjListXML = fromstring(rolistdata)
         relatedObjXML = relatedObjListXML.findall('./relation-list-item')
 
@@ -123,20 +126,26 @@ def eloan(request):
         objectNumbers = []
         for i in relatedObjXML:
 
-            objCsid = getfromXML(i,'./object/csid')
+            objCsid = getfromXML(i, './object/csid')
             objectCsids.append(objCsid)
 
             # Other options ...
             # if we ever need objectNumber and objectName
 
             # Note: ObjectNumber not a required field in UCJEPS instance, so using it for search is problematic
-            objNum = getfromXML(i,'./object/number')
-            # objNum = '' if objNum is None else objNum
+            objNum = getfromXML(i, './object/number')
+            # Search will fail for records with no object number so pass error message to screen
+            if objNum == '':
+                errMsg = 'Error: You have requested a loan that contains a record with no Specimen ID. Please have the herbarium staff check the loan.'
+                return render(request, 'eloan.html',
+                              {'loaninfo': loaninfo, 'results': errMsg, 'displayType': 'error', 'title': TITLE, 'timestamp': TIMESTAMP }
+                )
+                # else:
+            #     continue
             objectNumbers.append(objNum)
 
             # Note: ObjectName not necessarily unique, so using it for search is problematic
             # objName = getfromXML(i,'./object/name')
-            # objName = '' if objectName is None else objectName.text
             # objectNames.append(objName)
 
 
@@ -150,23 +159,32 @@ def eloan(request):
         # Build args to pass to solr
         # Static string parts of the URL
         solr_server = SOLRSERVER
-        solr_queryparamcsid = SOLRQUERYPARAMCSID
+        # solr_queryparamcsid = SOLRQUERYPARAMCSID
         solr_queryparamacc = SOLRQUERYPARAMACCESSION
         solr_core = SOLRCORE
 
-        # Pull individual CSIDs from list of objectCsids (i.e., CSIDs of objects related to the loan)
-        # Insert ' OR ' between each CSID
-        csidsToSearch = " OR ".join(objectCsids)
+        # Pull individual objectNumbers from list and insert ' OR ' between each
         objectNumbersToSearch = " OR ".join(objectNumbers)
-        query_dictionary = {solr_queryparamcsid:csidsToSearch, solr_queryparamacc:objectNumbersToSearch, 'displayType':'full', 'maxresults':'2000'}
-        solr_context = {'searchValues':query_dictionary}
-        # solr_context['resultsOnly'] = 'true'
+        # Perhaps do the same for CSIDs.
+        # csidsToSearch = " OR ".join(objectCsids)
+        query_dictionary = {solr_queryparamacc: objectNumbersToSearch, 'displayType': 'full', 'maxresults': '2000'}
+        #query_dictionary = {solr_queryparamcsid:csidsToSearch, solr_queryparamacc:objectNumbersToSearch, 'displayType':'full', 'maxresults':'2000'}
 
+        solr_context = {'searchValues': query_dictionary}
         results = doSearch(solr_server, solr_core, solr_context)
 
         # Either break values out of results array as we do here, or use results[value] notation in HTML templates
         return render(request, 'eloan.html',
-            {'loaninfo': loaninfo, 'results': results, 'items': results['items'], 'displayType': results['displayType'], 'count': results['count'], 'imageserver': results['imageserver'], 'url': results['url'], 'querystring': results['querystring'], 'timestamp': results['timestamp'], 'time': results['time'], 'kw': eloanNum})
+                      {'loaninfo': loaninfo, 'results': results, 'items': results['items'],
+                       'displayType': results['displayType'], 'count': results['count'],
+                       'imageserver': results['imageserver'], 'url': results['url'],
+                       'querystring': results['querystring'], 'timestamp': results['timestamp'],
+                       'time': results['time'], 'kw': eloanNum, 'title': TITLE }
+        )
 
     else:
-        return render(request, 'eloan.html', {'title': TITLE, 'timestamp': TIMESTAMP })
+        # In case we want to include a bit of instruction to the user.
+        msg = 'Please enter a loan number'
+        return render(request, 'eloan.html', {'title': TITLE, 'timestamp': TIMESTAMP, 'message': msg}
+        )
+
