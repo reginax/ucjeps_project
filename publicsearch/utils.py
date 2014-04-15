2d5eb8543df961e3dd9997972eab2626d1ca8937
jblowe <jblowe@berkeley.edu>
2014-04-15 15:23:14 -0700

    UCJEPS-502: implementing a new incremental version of compact.html

--- publicsearch/utils.py
+++ publicsearch/utils.py
@@ -67,7 +67,7 @@ PARMS = {
     'updatedat': ['Last updated at', 'true', '', 'updatedat_dt', ''],
     'previousdeterminations': ['Previous Determinations', 'true', '', 'previousdeterminations_ss', ''],
     'localname': ['Local Name', 'true', '', 'localname_txt', ''],
-    'briefdescription': ['Brief Description', 'true', '', 'briefdescription_txt', ''],
+    'briefdescription': ['Description', 'true', '', 'briefdescription_txt', ''],
     'associatedtaxa': ['Associated Taxa', 'true', '', 'associatedtaxa_ss', ''],
     'typeassertions': ['Type Assertions', 'true', '', 'typeassertions_ss', ''],
     'othernumbers': ['Other Numbers', 'true', '', 'othernumbers_ss', ''],
