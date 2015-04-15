59fd0aef0ef70fbff29a39f185b6f3d082c31648
John Lowe <jblowe@berkeley.edu>
2015-04-14 22:44:07 -0700

    UCJEPS-581: portal mods to support otherlocations_ss

--- publicsearch/appconfig.py
+++ publicsearch/appconfig.py
@@ -42,7 +42,7 @@ PARMS = {
     'earlycollectiondate': ['earlycollectiondate_dt', 'true', '', 'earlycollectiondate_dt', ''],
     'latecollectiondate': ['latecollectiondate', 'true', '', 'latecollectiondate_txt', ''],
     'locality': ['Locality', 'true', '', 'locality_txt', ''],
-    'otherlocalities': ['Other Localities', 'true', '', 'otherlocalities_s', ''],
+    'otherlocalities': ['Other Localities', 'true', '', 'otherlocalities_ss', ''],
     'localitynote': ['Locality Note', 'true', '', 'localitynote_txt', ''],
     'localitysource': ['Locality Source', 'true', '', 'localitysource_txt', ''],
     'localitysourcedetail': ['Locality Source Detail', 'true', '', 'localitysourcedetail_txt', ''],
