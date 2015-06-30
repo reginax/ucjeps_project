b1d01923ec69b00d1fc44768a0fc44a85e4e1403
reginax <regina.xu1@gmail.com>
2015-06-30 09:48:27 -0700

    UCJEPS-591: Removed url redirect for eloan and publicsearch

--- publicsearch/urls.py
+++ publicsearch/urls.py
@@ -4,8 +4,7 @@ from django.conf.urls import patterns, url
 from publicsearch import views
 
 urlpatterns = patterns('',
-                       url(r'^/?$', views.direct, name='direct'),
-                       url(r'^publicsearch/$', views.publicsearch, name='publicSearch'),
+                       url(r'^/?$', views.publicsearch, name='publicSearch'),
                        url(r'^embedded/$', views.embeddedsearch, name='embedded'),
                        url(r'^results/$', views.retrieveResults, name='retrieveResults'),
                        url(r'^bmapper/$', views.bmapper, name='bmapper'),
