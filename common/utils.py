561f0992e4ae1ba6b4b97c1b0e2631ce41091eed
John Lowe <jblowe@berkeley.edu>
2015-10-21 10:22:58 -0700

    PAHMA-1411: fix the handling of - in the 'elsewhere' case, which is the case for PAHMA's musno field

--- common/utils.py
+++ common/utils.py
@@ -123,6 +123,9 @@ def writeCsv(filehandle, fieldset, items, writeheader=False, csvFormat='csv'):
             for x in item['otherfields']:
                 if x['name'] not in fieldset:
                     continue
+                if type(x['value']) == type([]):
+                    x['value'] = '|'.join(x['value'])
+                    pass
                 r.append(checkValue(x['value']))
             location = item['location']
             l = location.split(',')
@@ -456,6 +459,8 @@ def doSearch(context, prmz):
                 else:
                     if p in prmz.DROPDOWNS:
                         # if it's a value in a dropdown, it must always be an "exact search"
+                        # only our own double quotes are unescaped
+                        t = t.replace('"','\\"')
                         t = '"' + t + '"'
                         index = prmz.PARMS[p][3].replace('_txt', '_s')
                     elif p + '_qualifier' in requestObject:
@@ -469,32 +474,43 @@ def doSearch(context, prmz):
                             # for exact searches, reset the index to the original in case the switcharoo changed it
                             index = prmz.PARMS[p][3]
                             index = index.replace('_txt', '_s')
+                            # only our own double quotes are unescaped
+                            t = t.replace('"','\\"')
                             t = '"' + t + '"'
                         elif qualifier == 'phrase':
-                            index = index.replace('_ss', '_txt')
-                            index = index.replace('_s', '_txt')
+                            index = index.replace('_ss', '_txt').replace('_s', '_txt')
+                            # only our own double quotes are unescaped
+                            t = t.replace('"', '\\"')
                             t = '"' + t + '"'
                         elif qualifier == 'keyword':
+                            # eliminate some characters that might confuse solr's query parser
+                            t = re.sub(r'[\[\]\:\(\)\" ]', ' ', t).strip()
+                            # hyphen is allowed, but only as a negation operator
+                            t = re.sub(r'([^ ])-', '\1 ', ' ' + t).strip()
+                            # get rid of muliple spaces in a row
+                            t = re.sub(r' +', ' ', t)
                             t = t.split(' ')
                             t = ' +'.join(t)
                             t = '(+' + t + ')'
                             t = t.replace('+-', '-')  # remove the plus if user entered a minus
-                            index = index.replace('_ss', '_txt')
-                            index = index.replace('_s', '_txt')
+                            index = index.replace('_ss', '_txt').replace('_s', '_txt')
                     elif '_dt' in prmz.PARMS[p][3]:
                         querypattern = '%s: "%sZ"'
                         index = prmz.PARMS[p][3]
                     else:
-                        t = t.split(' ')
-                        t = ' +'.join(t)
-                        t = '(+' + t + ')'
-                        t = t.replace('+-', '-')  # remove the plus if user entered a minus
+                        # if no search qualifier is specified use the 'phrase' approach, copied from above
+                        # eliminate some characters that might confuse solr's query parser
                         index = prmz.PARMS[p][3]
+                        #index = index.replace('_ss', '_txt').replace('_s', '_txt')
+                        # only our own double quotes are unescaped
+                        t = t.replace('"', '\\"')
+                        #t = '"' + t + '"'
                 if t == 'OR': t = '"OR"'
                 if t == 'AND': t = '"AND"'
                 ORs.append(querypattern % (index, t))
             searchTerm = ' OR '.join(ORs)
             if ' ' in searchTerm and not '[* TO *]' in searchTerm: searchTerm = ' (' + searchTerm + ') '
+            # print searchTerm
             queryterms.append(searchTerm)
             urlterms.append('%s=%s' % (p, cgi.escape(requestObject[p])))
             if p + '_qualifier' in requestObject:
@@ -526,7 +542,7 @@ def doSearch(context, prmz):
     else:
         locsonly = None
 
-    print 'Solr query: %s' % querystring
+    # print 'Solr query: %s' % querystring
     try:
         startpage = context['maxresults'] * (context['start'] - 1)
     except:
