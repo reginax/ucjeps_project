142efb7c57fa7df757b29dc34b4428c4df1ed389
John Lowe <jblowe@berkeley.edu>
2016-02-03 16:41:26 -0800

    UCJEPS-614: initial version of media portal

--- cspace_django_site/extra_prod.py
+++ cspace_django_site/extra_prod.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = (
     'ireports',
     'landing',
     'search',
+    'searchmedia',
     'publicsearch',
     #'toolbox',
     'simplesearch',
--- cspace_django_site/extra_pycharm.py
+++ cspace_django_site/extra_pycharm.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = (
     'ireports',
     'landing',
     'search',
+    'searchmedia',
     'publicsearch',
     #'toolbox',
     'simplesearch',
