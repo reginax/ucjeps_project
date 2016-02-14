e269760e3bc54aaf3a601a505a5dcf4b4b4016f0
John Lowe <jblowe@berkeley.edu>
2016-02-13 23:01:03 -0800

    NOJIRA: update apps and setup for this project

--- setup.sh
+++ setup.sh
@@ -13,16 +13,26 @@ elif [ "$COMMAND" = "show" ]; then
     echo -e "from cspace_django_site.extra_settings import INSTALLED_APPS\nfor i in INSTALLED_APPS: print i" | python
     echo
 elif [ "$COMMAND" = "deploy" ]; then
-    cp ../django_example_config/$2/* config
-    cp config/main.cfg cspace_django_site
+    rm config/*.cfg
+    rm config/*.csv
+    rm config/*.xml
+    cp ~/django_example_config/$2/* config
+    mv config/main.cfg cspace_django_site
+    rm fixtures/*.json
     mv config/*.json fixtures
-    cd cspace_django_site/static/cspace_django_site/images
-    cp header-logo-$2.png header-logo.png
+    # just to be sure, we start over with this...
+    rm db.sqlite3
+    python manage.py syncdb --noinput
+    python manage.py migrate
+    python manage.py loaddata fixtures/*.json
+    cp cspace_django_site/static/cspace_django_site/images/header-logo-$2.png cspace_django_site/static/cspace_django_site/images/header-logo.png
+    python manage.py collectstatic --noinput
+    echo "Don't forget to configure cspace_django_site/main.cfg and the rest of the configuration files in config/"
 elif [ "$COMMAND" = "configure" ]; then
     cp cspace_django_site/extra_$2.py cspace_django_site/extra_settings.py
     cp cspace_django_site/all_urls.py cspace_django_site/urls.py
+    rm db.sqlite3
     python manage.py syncdb --noinput
+    python manage.py migrate
     python manage.py collectstatic --noinput
-    python manage.py loaddata fixtures/*.json
 fi
-echo "Don't forget to configure main.cfg and the rest of the configuration files in config/"
